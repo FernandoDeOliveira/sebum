73e056d70d60d9ba8f8627cafcb4d96f8dcc7da3
FernandoDeOliveira <linhares.lfoliveira@gmail.com>
2019-06-03 19:20:33 -0300

    refactored name file

--- lib/pages/root_page.dart
+++ lib/pages/root_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sebum/pages/login_page.dart';
+import 'package:sebum/pages/login_signup_page.dart';
 import 'package:sebum/services/authentication.dart';
 import 'package:sebum/pages/home_page.dart';
 
