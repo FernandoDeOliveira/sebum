5712d36db000404a8d6e8cfb74fcafcc16ce41bf
Vanessa Oliveira <vanessaoliveira2706@gmail.com>
2019-06-07 10:52:04 -0300

    rotas alterações

--- lib/pages/bookcase.dart
+++ lib/pages/bookcase.dart
@@ -34,54 +34,6 @@ class _MyPage extends State<Page> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      drawer: new Drawer(
-        child: new ListView(
-          children: <Widget>[
-            new UserAccountsDrawerHeader(
-              accountName: new Text("Vanessa Oliveira"),
-              accountEmail: new Text("vanessaoliveira2706@gmail.com"),
-              currentAccountPicture: new GestureDetector(
-                child: new CircleAvatar( backgroundImage: AssetImage('assets/images/foto.jpeg'),
-                  
-                ),
-              ),
-            ),
-             new ListTile(
-              title: new Text("Meu Perfil"),
-              trailing: new Icon(Icons.arrow_right),
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ProfilePage())
-                  );
-              }
-            ),
-             new ListTile(
-              title: new Text("Minha Estante"),
-              trailing: new Icon(Icons.arrow_right),
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Bookcase())
-                  );
-              }
-            ),
-             new ListTile(
-              title: new Text("Sair"),
-              trailing: new Icon(Icons.arrow_right),
-              onTap: () {
-                
-              }
-            ),
-            new Divider(),
-            new ListTile(
-              title: new Text("Fechar"),
-              trailing: new Icon(Icons.cancel),
-              onTap: () {Navigator.pop(context);},
-            ),
-          ],
-        ),
-      ),
 
       body: ListView(
         shrinkWrap: true,
