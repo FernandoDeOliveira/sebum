bc21d69e4f5cbf2e48f0b1704149ba207433afc6
Vanessa Oliveira <vanessaoliveira2706@gmail.com>
2019-06-21 00:26:49 -0300

    sidemenu em pagina separada e cards p/ busca

---
+++ lib/pages/booksearch.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/painting.dart';
+import 'package:sebum/pages/login_signup_page.dart';
+
+class BookSearch extends StatefulWidget {
+  @override
+  _BookSearchState createState() => _BookSearchState();
+}
+
+class _BookSearchState extends State<BookSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+
+Widget _buildCard(book){
+  return Card(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(10.0)
+      ),
+    child: Column(
+      children: <Widget>[
+        Stack(
+          Container(
+            alignment: ,)
+
+        )
+      ],
+    ),
+  )
+}
+
+
+}
