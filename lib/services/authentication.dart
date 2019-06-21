fccd70673a47ed49f84ec14faa6e86327bb3bad5
FernandoDeOliveira <linhares.lfoliveira@gmail.com>
2019-06-21 03:58:42 -0300

    transferência de metodos para classe DB no firestoreDB.dart

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -2,15 +2,12 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sebum/models/book.dart';
+import 'package:sebum/models/user.dart';
 
 abstract class BaseAuth {
   Future<String> signIn(String email, String password);
 
   Future<String> signUp(String email, String password);
-
-  Future<void> addNameUser(String name);
-
-//  Future<String> getNameUser();
 
   Future<FirebaseUser> getCurrentUser();
 
@@ -18,8 +15,6 @@ abstract class BaseAuth {
 
 
   Future<void> sendEmailVerification();
-
-  Future<void> add_to_bookcase(Book book);
 
 
   Future<bool> isEmailVerified();
@@ -39,6 +34,7 @@ class Auth implements BaseAuth {
   Future<String> signUp(String email, String password) async {
     user = await _firebaseAuth.createUserWithEmailAndPassword(
         email: email, password: password);
+
     db.collection('users').document().setData({'email': email});
     return user.uid;
   }
@@ -48,11 +44,9 @@ class Auth implements BaseAuth {
     return user;
   }
 
-
   Future<void> signOut() async {
     return _firebaseAuth.signOut();
   }
-
 
   Future<void> sendEmailVerification() async {
     FirebaseUser user = await _firebaseAuth.currentUser();
@@ -63,30 +57,5 @@ class Auth implements BaseAuth {
     FirebaseUser user = await _firebaseAuth.currentUser();
     return user.isEmailVerified;
   }
-
-  Future<void> add_to_bookcase(Book book) async {
-    FirebaseUser user = await _firebaseAuth.currentUser();
-    var data = await book.data_like_json();
-    String userid = user.uid.toString();
-    db.collection('users').document(
-        userid).collection('bookcase').add(book.data_like_json());
-  }
-
-  Future<void> addNameUser(String name) async {
-    FirebaseUser user = await _firebaseAuth.currentUser();
-    String userid = user.uid.toString();
-    db.collection('users').document(
-      userid).setData({'name': name});
-  }
-
-  /*Future<String> getNameUser() async {
-    FirebaseUser user = await _firebaseAuth.currentUser();
-    String userid = user.uid.toString();
-    String name;
-    name = db.collection('users').document(
-      userid).get().then((snap) => {
-      snap.do
-    });
-  }*/
 
 }
