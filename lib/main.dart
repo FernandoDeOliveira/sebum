36b06e1280ba8dba9ccc7a148d35f3b8607816f5
Vanessa Oliveira <vanessaoliveira2706@gmail.com>
2019-06-21 11:08:31 -0300

    modificações para teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sebum/pages/bookcase.dart';
 import 'package:sebum/pages/home_page.dart';
-import 'package:sebum/pages/partials/book_shearch_widget.dart';
+import 'package:sebum/pages/partials/book_search_widget.dart';
+import 'package:sebum/pages/profile_page.dart';
 import 'package:sebum/pages/root_page.dart';
 import 'package:sebum/services/authentication.dart';
 
