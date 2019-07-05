1ec065d00c98f67a62fa1e0afdc40999e918622e
FernandoDeOliveira <linhares.lfoliveira@gmail.com>
2019-07-05 14:35:25 -0300

    modificação p/ passar userId para profile_page.dart

--- lib/pages/root_page.dart
+++ lib/pages/root_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:sebum/models/user.dart';
 import 'package:sebum/pages/login_signup_page.dart';
 import 'package:sebum/pages/profile_page.dart';
 import 'package:sebum/services/authentication.dart';
-import 'package:sebum/pages/home_page.dart';
-import 'package:sebum/services/firestoreDB.dart';
 
 class RootPage extends StatefulWidget {
   RootPage({this.auth});
@@ -81,9 +78,8 @@ class _RootPageState extends State<RootPage> {
         break;
       case AuthStatus.LOGGED_IN:
         if (_userId.length > 0 && _userId != null) {
-          Future<User> user = DB().getUser(_userId);
           return new ProfilePage(
-            user: user,
+            userId: _userId,
             auth: widget.auth,
             onSignedOut: _onSignedOut,
 
