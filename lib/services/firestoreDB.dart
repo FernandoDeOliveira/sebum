fa38067e91ef1e9beea477d56c21528361385d62
FernandoDeOliveira <linhares.lfoliveira@gmail.com>
2019-07-05 23:52:42 -0300

    criado metodos: addToUserBookcase, getBook, getUsersBooks

--- lib/services/firestoreDB.dart
+++ lib/services/firestoreDB.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sebum/models/book.dart';
+import 'package:sebum/models/todo.dart';
 import 'package:sebum/models/user.dart';
 
 abstract class BaseFirebase{
@@ -10,9 +11,13 @@ abstract class BaseFirebase{
 
 //  Future<void> changeUserName(String name);
 
-  Future<void> addToUserBookcase(String userId, Book book);
+  addToUserBookcase(String userId, String bookId);
 
   Future<User> getUser(String userId);
+
+  Future<Book> getBook(String bookId);
+  
+  Future<List<Book>> getUserBooks(String userId);
 
 }
 
@@ -21,21 +26,54 @@ class DB implements BaseFirebase{
   FirebaseUser user;
 
   Future<User> getUser(String userId) async {
-    var docRef = await db.collection('users').document('s1AaKr1kluh2auQvGsqBhF91JBj2');
+    var docRef = await db.collection('users').document(userId);
     var data = await docRef.get().then((document) => document.data);
     User user = User();
-    print('********************************************') ;
-    print(data) ;
-    print('********************************************') ;
     user.fromMap(data);
-    print(user.name);
     return user;
   }
 
-  Future<void> addToUserBookcase(String userId, Book book) async {
-    var data = await book.dataLikeJson();
-    db.collection('users').document(
-        userId).collection('bookcase').add(book.dataLikeJson());
+  addToUserBookcase(String userId, String bookId) {
+  db.collection('books').document(bookId).get().then(
+      (book) {
+
+         db.collection('books').
+        document(bookId).setData({
+           'usersOwningTheBooks': book.data['usersOwningTheBooks'] + [userId]},
+             merge: true);
+      }
+  );
+
   }
+
+  Future<Book> getBook(String bookId) async{
+    var docRef = await db.collection('books').document(bookId);
+    var data = await docRef.get().then((document) => document.data);
+    Book book = Book();
+    book.fromMap(data);
+    return book;
+  }
+  
+  Future<List<Book>> getUserBooks(String userId) async{
+    List<Map<String,dynamic>> booksAsJson = List<Map<String,dynamic>>();
+
+    var docRef = await db.collection('books')
+        .where('usersOwningTheBooks', arrayContains: userId);
+
+    await docRef.getDocuments().then(
+            (docs) => docs.documents.forEach(
+                (doc) =>booksAsJson.add(doc.data))
+            );
+
+    List<Book> books = List<Book>();
+    for (var b in booksAsJson){
+      Book book = Book();
+      book.fromMap(b);
+      books.add(book);
+    }
+
+    return books;
+  }
+
 
 }
