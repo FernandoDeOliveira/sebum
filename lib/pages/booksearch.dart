775c45329cf3617fee80ff8a239e625deb4ac0d9
Vanessa Oliveira <vanessaoliveira2706@gmail.com>
2019-07-09 21:39:47 -0300

    card para page de encontrar livros de usuarios criado

--- lib/pages/booksearch.dart
+++ lib/pages/booksearch.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:sebum/pages/login_signup_page.dart';
-import 'package:sebum/models/mocks.dart/';
+import 'package:sebum/models/mocks.dart';
 
 class BookSearch extends StatefulWidget {
   @override
@@ -59,6 +59,72 @@ Widget _buildCard(book){
     
   );
 }
+ Widget _buildCardUserBooks(book){
+   return Card(
+     shape: RoundedRectangleBorder(
+       borderRadius: BorderRadius.circular(10.0)
+     ),
+     child: new Container(
+       child: new Column(
+         children: <Widget>[
+           new Row(
+             children: <Widget>[
+               new Column(
+                 children: <Widget>[
+                   new Row(
+                     children: <Widget>[
+                       Text(book.title),
+                     ],
+                    ),
+                    new Row(
+                      children: <Widget>[
+                        Text(book.author)
+                      ],
+                    )
+                 ],
+                ),
+                new Column(
+                  children: <Widget>[
+                    new Container(
+                    height: 200.0,
+                    width: 400.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30.0),
+                      image: DecorationImage(
+                        image: NetworkImage(book.url_photo))
+                      ),
+                    ),
+                  ],
+                )
+             ],
+            ),
+            new Row( //fazer iteração com essa row pra aparecer os 3 primeiros usuarios do banco que possuem o livro
+              children: <Widget>[
+                new Container(
+                  alignment: Alignment.centerLeft,
+                  child: new Text("Usuario") //Nome do usuario que possui livro
+                ),
+                new Container(
+                  alignment: Alignment.centerRight,
+                  child:
+                    FlatButton.icon(
+                    color: Colors.purple[200],
+                    label: Text("Pedir emprestado"),
+                    icon: Icon(Icons.check),
+                    onPressed: (){
+
+                     },
+                    )
+                  )
+              ],
+            ),
+
+         ],
+         )
+         ,) 
+   );
+ }
+
 
 
 }
